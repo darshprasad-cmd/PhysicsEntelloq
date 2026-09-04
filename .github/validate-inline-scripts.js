4e0d225b8028a2b77ec2b9774ef59fac5479d242
darshprasad-cmd <darshprasad@gmail.com>
2026-09-04 22:27:36 +0530

    Make mastery the primary learning path (#2)
    * Make mastery the primary learning path
    
    * Add required static site check
    
    * Make site validator location independent

---
+++ .github/validate-inline-scripts.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
+const html = fs.readFileSync(path.join(__dirname, '..', 'index.html'), 'utf8');
+const scripts = [...html.matchAll(/<script(?:\s[^>]*)?>([\s\S]*?)<\/script>/gi)];
+let parsed = 0;
+
+for (const match of scripts) {
+  const openingTag = match[0].slice(0, match[0].indexOf('>'));
+  if (/\bsrc\s*=/.test(openingTag)) continue;
+  new Function(match[1]);
+  parsed += 1;
+}
+
+if (!parsed) throw new Error('No inline scripts found');
+console.log(`Parsed ${parsed} inline scripts.`);
